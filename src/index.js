01c10ccd77a35c47814a23cb00db388349163608
Kethan VA <kethan@netreo.com>
2020-07-28 19:36:10 +0530

    Added few changes to import image and profile

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import "./index.css";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./screens/login/Login";
+import Home from "./screens/home/Home";
 
 ReactDOM.render(
   <Router>
     <Route exact path="/" component={Login} />
+    <Route exact path="/home" component={Home} />
   </Router>,
   document.getElementById("root")
 );
