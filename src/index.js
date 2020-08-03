5b205d95ad3a563b36fe72ab6edfe8908682442c
Kethan VA <kethan@netreo.com>
2020-08-03 19:39:56 +0530

    Test

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import Login from "./screens/login/Login";
 import Home from "./screens/home/Home";
 
 ReactDOM.render(
-  <Router>
-    <Route exact path="/" component={Login} />
-    <Route exact path="/home" component={Home} />
-  </Router>,
-  document.getElementById("root")
+    <Router>
+        <Route exact path="/" component={Login}/>
+        <Route exact path="/home" component={Home}/>
+    </Router>,
+    document.getElementById("root")
 );
