0828c1191b3741b21891f588147dc499109a3be9
Kethan VA <kethan@netreo.com>
2020-08-07 04:31:07 +0530

    Final checkin to fix all issues for module 2 test

--- src/common/Properties.js
+++ src/common/Properties.js
@@ -1,6 +1,7 @@
 const properties = {
     username: "admin",
     password: "admin",
+    profilePhoto: "https://instagram.fblr8-1.fna.fbcdn.net/v/t51.2885-19/s320x320/109953852_968760010227212_5963952208363216611_n.jpg?_nc_ht=instagram.fblr8-1.fna.fbcdn.net&_nc_ohc=RVIGz4KkQ4QAX-kLqo9&oh=fb9633581d8cf1350b9d5e8ee21c54d9&oe=5F57B5EB",
     accessToken: "IGQVJXRzlwbjM0R0JURXd6WDA5S0hFU0JGYUVGTURRdXNYZAWNmUlotbEZAjLWdJQ2FZAbkVvU0R5b05JVWV6NExoUGdHXy1vdWxyMm4zeWxtT0pwNjZAxa0pvUmhPNXJyX3VIQTZALcmE1RFV0T1BZATkZAXTDJIdVdhQjZAoMUFr"
 
 }
--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,101 +1,103 @@
 import * as React from "react";
 import {
-  Card,
-  FormControl,
-  Input,
-  InputLabel,
-  Button
+    Card,
+    FormControl,
+    Input,
+    InputLabel,
+    Button
 } from "@material-ui/core";
-import Header from "../../common/Header";
 import "./Login.css";
 import properties from "../../common/Properties";
 
 export default class Login extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      username: "",
-      password: "",
-      validateCredentials: true,
-      emptyUsername: false,
-      emptyPassword: false
+    constructor() {
+        super();
+        this.state = {
+            username: "",
+            password: "",
+            validateCredentials: true,
+            emptyUsername: false,
+            emptyPassword: false
+        };
+    }
+
+    handleEvent(e, type) {
+        const value = e.target.value;
+        const nextState = {};
+        nextState[type] = value;
+        this.setState(nextState);
+    }
+
+    handleClick = () => {
+        const user = properties.username;
+        const pwd = properties.password;
+        const {username, password} = this.state;
+        if (username !== "" || password !== "") {
+            this.setState({
+                emptyUsername: false,
+                emptyPassword: false,
+                validateCredentials: true
+            });
+            if (username === "") {
+                this.setState({emptyUsername: true});
+                return;
+            }
+            if (password === "") {
+                this.setState({emptyPassword: true});
+                return;
+            }
+        } else {
+            this.setState({emptyUsername: true, emptyPassword: true});
+            return;
+        }
+        if (username === user && password === pwd) {
+            this.setState({validateCredentials: true});
+            sessionStorage.setItem(
+                "accessToken",
+                properties.accessToken
+            );
+            window.location = "/home";
+        } else {
+            this.setState({validateCredentials: false});
+        }
     };
-  }
-  handleEvent(e, type) {
-    const value = e.target.value;
-    const nextState = {};
-    nextState[type] = value;
-    this.setState(nextState);
-  }
-  handleClick = () => {
-    const user = properties.username;
-    const pwd = properties.password;
-    const { username, password } = this.state;
-    if (username !== "" || password !== "") {
-      this.setState({
-        emptyUsername: false,
-        emptyPassword: false,
-        validateCredentials: true
-      });
-      if (username === "") {
-        this.setState({ emptyUsername: true });
-        return;
-      }
-      if (password === "") {
-        this.setState({ emptyPassword: true });
-        return;
-      }
-    } else {
-      this.setState({ emptyUsername: true, emptyPassword: true });
-      return;
+
+    render() {
+        const {validateCredentials, emptyUsername, emptyPassword} = this.state;
+        return (
+            <div className="login-wrapper">
+                <Card className="login-hoarding">
+                    <h3 className="login-heading">LOGIN</h3>
+                    <FormControl fullWidth={true} margin="normal">
+                        <InputLabel htmlFor="username">Username *</InputLabel>
+                        <Input
+                            id="username"
+                            onChange={e => this.handleEvent(e, "username")}
+                        />
+                        {emptyUsername ? <span className="error">required</span> : null}
+                    </FormControl>
+                    <FormControl fullWidth={true} margin="normal">
+                        <InputLabel htmlFor="password">Password *</InputLabel>
+                        <Input
+                            id="password"
+                            type="password"
+                            onChange={e => this.handleEvent(e, "password")}
+                        />
+                        {emptyPassword ? <span className="error">required</span> : null}
+                        {!validateCredentials ? (
+                            <span className="error">Incorrect username and/or password</span>
+                        ) : null}
+                    </FormControl>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        className="login-button in1"
+                        onClick={this.handleClick}
+                    >
+                        LOGIN
+                    </Button>
+                </Card>
+            </div>
+        );
     }
-    if (username === user && password === pwd) {
-      this.setState({ validateCredentials: true });
-      sessionStorage.setItem(
-        "accessToken",
-          properties.accessToken
-      );
-      window.location = "/home";
-    } else {
-      this.setState({ validateCredentials: false });
-    }
-  };
-  render() {
-    const { validateCredentials, emptyUsername, emptyPassword } = this.state;
-    return (
-      <div className="login-wrapper">
-        <Card className="login-hoarding">
-          <h3 className="login-heading">LOGIN</h3>
-          <FormControl fullWidth={true} margin="normal">
-            <InputLabel htmlFor="username">Username *</InputLabel>
-            <Input
-              id="username"
-              onChange={e => this.handleEvent(e, "username")}
-            />
-            {emptyUsername ? <span className="error">required</span> : null}
-          </FormControl>
-          <FormControl fullWidth={true} margin="normal">
-            <InputLabel htmlFor="password">Password *</InputLabel>
-            <Input
-              id="password"
-              type="password"
-              onChange={e => this.handleEvent(e, "password")}
-            />
-            {emptyPassword ? <span className="error">required</span> : null}
-            {!validateCredentials ? (
-              <span className="error">Incorrect username and/or password</span>
-            ) : null}
-          </FormControl>
-          <Button
-            variant="contained"
-            color="primary"
-            className="login-button in1"
-            onClick={this.handleClick}
-          >
-            LOGIN
-          </Button>
-        </Card>
-      </div>
-    );
-  }
 }
