9201b16d71642331a5560282b6a93130ad826af1
justanotherprogrammerhere <kethan.inbox@gmail.com>
2020-08-07 04:38:01 +0530

    Session2  branhc (#16)
    * Files
    
    * Added important files to enable homepage with details
    
    * Files
    
    * Final checkin to fix all issues for module 2 test
    
    * Final checkin to fix all issues for module 2 test
    
    Co-authored-by: Kethan VA <kethan@netreo.com>

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,29 +1,43 @@
 import React from "react";
 import Header from "../../common/Header";
-import {Avatar, Button, Card, CardContent, CardHeader, FormControl, Input, InputLabel} from "@material-ui/core";
+import {
+    Avatar,
+    Button,
+    Card,
+    IconButton,
+    CardContent,
+    CardHeader,
+    FormControl,
+    Input,
+    InputLabel
+} from "@material-ui/core";
 import "./Home.css";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import properties from "../../common/Properties";
 
 const API2 = 'https://graph.instagram.com/me/media?fields=id,caption&access_token=' + properties.accessToken;
-const API5 = 'https://graph.instagram.com/'
+const API5 = 'https://graph.instagram.com/';
 const API6 = '?fields=media_type,media_url,username,timestamp&access_token=' + properties.accessToken;
 
-const MANUAL_API1 = 'https://raw.githubusercontent.com/justanotherprogrammerhere/InstaImageViewer/master/Test/Data/all_posts.json'
-const MANUAL_API2 = 'https://raw.githubusercontent.com/justanotherprogrammerhere/InstaImageViewer/master/Test/Data/'
-const MANUAL_API3 = '/photo_details.json'
-//import mainLogo from'./logoWhite.png';
+const MANUAL_API1 = 'https://raw.githubusercontent.com/justanotherprogrammerhere/InstaImageViewer/master/Test/Data/all_posts.json';
+const MANUAL_API2 = 'https://raw.githubusercontent.com/justanotherprogrammerhere/InstaImageViewer/master/Test/Data/';
+const MANUAL_API3 = '/photo_details.json';
 
 export default class Home extends React.Component {
     constructor() {
         super();
         this.state = {
-            imagesData: [],
-            filterImages: [],
             photos: [],
-            photo_details: [],
-            liked: false
+            photoDetails: [],
+            photoDetailsUnAltered: [],
+            profilePhoto: properties.profilePhoto,
+            comment: null,
+            comments: [],
+            commentForImage: [],
+            likedByUser: [],
+            searchOn: false,
+            userLiked: false
         };
     }
 
@@ -59,92 +73,185 @@ export default class Home extends React.Component {
                 })
                 .then(data => {
                     if (data.media_url) {
-                        data.caption = photo.caption
-                        data.tags = '#testtag1 #testag2'
+                        data.caption = photo.caption;
+                        //Manually creating likes
+                        data.likes = Math.floor(Math.random() * 100);
+                        data.tags = '#testtag1 #testag2';
                         this.setState({
-                            photo_details: [...this.state.photo_details, data], photoDetailsLoading: false
+                            photoDetails: [...this.state.photoDetails, data], photoDetailsLoading: false
                         });
                     }
                 })
                 .catch(error => this.setState({error, photoDetailsLoading: false}))
         )
 
+
     }
 
-    filterData = str => {
-        const {imagesData} = this.state;
-        if (imagesData) {
-            const result = imagesData.filter(imageData =>
-                imageData.caption.text.includes(str)
-            );
-            this.setState({filterImages: result});
+
+    dateConvertor = timeStamp => {
+        var resTs = timeStamp.replace('/-', '//');
+        resTs = resTs.replace('T', ' ');
+        resTs = resTs.replace('+', ' ');
+        resTs = resTs.slice(0,resTs.length-4);
+        return resTs;
+
+    };
+
+    loadPost = (index) => {
+        this.state.likedByUser[index] = false;
+        this.forceUpdate();
+    };
+
+    filterPhotos = str => {
+        const {photoDetails, searchOn} = this.state;
+        if (searchOn === false) {
+            if (str !== "" || str !== null) {
+                this.setState({photoDetailsUnAltered: this.state.photoDetails});
+                this.setState({searchOn: true});
+            }
+        } else {
+            if (str === "" || str === null) {
+                this.setState({photoDetails: this.state.photoDetailsUnAltered});
+                this.setState({searchOn: false});
+                return;
+            }
+        }
+
+        if (photoDetails) {
+            const result = photoDetails
+                .filter(imageData => imageData.caption.includes(str))
+                .map(imageData => imageData);
+            this.setState({photoDetails: result});
         }
     };
-    dateConvertor = timeStamp => {
-        let newDate = new Date(timeStamp);
-        let year = newDate.getFullYear();
-        let monthNo = newDate.getMonth() + 1;
-        let date = newDate.getDate();
-        let hour = newDate.getHours();
-        let min = newDate.getMinutes();
-        let sec = newDate.getSeconds();
-        hour = hour % 12;
-        hour = hour || 12;
-        min = min < 10 ? "0" + min : min;
-        date = date < 10 ? "0" + date : date;
-        monthNo = monthNo < 10 ? "0" + monthNo : monthNo;
-        let time = hour + ":" + min + ":" + sec;
-        let dateObj = {
-            time: time,
-            calSlashDate: date + "/" + monthNo + "/" + year.toString()
-        };
-        return dateObj.calSlashDate + " " + dateObj.time;
-    };
-    liked = id => {
-        const {filterImages} = this.state;
-        const likedImage = filterImages.filter(img => img.id === id);
-        const updateFilterImages = filterImages.map(img => {
-            var returnValue = {...img};
-            if (img.id === likedImage.id) {
-                returnValue["liked"] = true;
+
+    likeBtnHandler = (imageId) => {
+        var i = 0
+        var imageArray = this.state.photoDetails;
+        for (i; i < imageArray.length; i++) {
+            if (imageArray[i].id === imageId) {
+                if (imageArray[i].userLiked === true) {
+                    imageArray[i].userLiked = false;
+                    imageArray[i].likes--;
+                    this.setState({
+                        images: imageArray
+                    });
+                    break;
+                } else {
+                    imageArray[i].userLiked = true;
+                    imageArray[i].likes++;
+                    this.setState({
+                        images: imageArray
+                    });
+                    break;
+                }
             }
-            return returnValue;
-        });
-        console.log(updateFilterImages);
+        }
+    };
+
+    commentHandler = (event, index) => {
+        //console.log(this.state.commentForImage[index])
+        this.setState({comment: event.target.value});
+        this.state.commentForImage[index] = event.target.value;
+        this.forceUpdate();
+    };
+
+    addCommentHandler = (index) => {
+        if (this.state.comment !== null && this.state.comment !== "") {
+            if (this.state.comments[index] === undefined)
+                this.state.comments[index] = this.state.comment;
+            else
+                this.state.comments[index] = this.state.comments[index] + ":" + this.state.comment;
+            console.log(this.state.comments);
+            this.forceUpdate();
+
+            this.setState({comment: ''});
+            this.state.commentForImage[index] = "";
+            this.forceUpdate();
+
+        }
     };
 
     render() {
-        const {photos, photo_details} = this.state;
+        const {photos, photoDetails, profilePhoto} = this.state;
         return (
             <React.Fragment>
                 <div>
                     <Header
                         homepageHeader={true}
-                        onFilter={this.photo_details}
+                        onFilter={this.filterPhotos}
                     />
                     <div className="card-wrapper">
-                        {photo_details && photo_details.length > 0 ? photo_details.map(img => (
-                                <Card className="card" key={img.id}>
+                        {photoDetails && photoDetails.length > 0 ? photoDetails.map((imgDetail, index) => (
+                                <Card className="card" key={imgDetail.id}>
+
+                                    <CardHeader
+                                        avatar={
+                                            <Avatar aria-label="recipe">
+                                                <img src={this.state.profilePhoto}
+                                                     alt="user"
+                                                     className="profile-pic"
+                                                />
+                                            </Avatar>
+                                        }
+                                        title={imgDetail.username}
+                                        subheader={this.dateConvertor(imgDetail.timestamp)}
+                                    />
+
                                     <CardContent>
                                         <div className="content">
-                                            {console.log(img.media_url)}
-                                            <img src={img.media_url}
+                                            <img src={imgDetail.media_url}
                                                  alt="post-photo"
                                                  className="post-photo"/>
                                             <hr/>
-                                            {img.caption ?
-                                                <div className="caption"> {img.caption}</div> :
+
+                                            {imgDetail.caption ?
+                                                <div className="caption"> {imgDetail.caption}</div> :
                                                 <div className="caption"> &nbsp;</div>}
+                                            <span className={"small-padding"}></span>
+
+                                            <div>
+                                                <IconButton className="like-button" aria-label="like-button"
+                                                            onClick={() =>
+                                                                this.likeBtnHandler(imgDetail.id)}> {imgDetail.userLiked ?
+                                                    <FavoriteIcon className="image-liked-icon" fontSize="large" color="secondary"/> :
+                                                    <FavoriteBorderIcon className="image-like-icon" fontSize="large"/>}
+                                                </IconButton>
+                                                {imgDetail.likes === 1 ?
+                                                    <span> {imgDetail.likes} like </span> :
+                                                    <span> {imgDetail.likes} likes </span>
+                                                }
+                                            </div>
+                                            <span className={"small-padding"}></span>
+                                            <div className="comment-container medium-padding">
+                                                {this.state.comments[index] !== undefined && this.state.comments[index] !== null ?
+                                                    this.state.comments[index].split(':').map(
+                                                        comment => (<div key={imgDetail.id}>
+                                                            <span className="text-bold">{imgDetail.username} : </span>
+                                                            <span>{comment}</span><br/>
+                                                        </div>)) : ""}
+
+                                                <FormControl>
+                                                    <div className="comment-section">
+                                                        <InputLabel htmlFor={"comment" + imgDetail.id}>Add a
+                                                            comment</InputLabel>
+                                                        <Input key={"comment" + imgDetail.id} type="text"
+                                                               value={this.state.commentForImage[index]}
+                                                               comment={this.state.comment} onChange={(e) => {
+                                                            this.commentHandler(e, index)
+                                                        }}/>
+                                                        <Button variant="contained" color="primary"
+                                                                onClick={() => this.addCommentHandler(index)}>
+                                                            ADD
+                                                        </Button>
+
+                                                    </div>
+                                                </FormControl>
+
+                                            </div>
 
 
-                                            <FormControl
-                                                fullWidth={true}
-                                                margin="normal"
-                                                className="comment-form">
-                                                <InputLabel htmlFor="comment">Add a comment</InputLabel>
-                                                <Input className="comment-input"/>
-                                                <Button variant="contained" color="primary"> ADD </Button>
-                                            </FormControl>
                                         </div>
                                     </CardContent>
                                 </Card>
@@ -152,27 +259,6 @@ export default class Home extends React.Component {
                             : null}
 
                     </div>
-
-
-                    <div>
-                        <ul>
-                            <div id={photos.size}>
-                                {photos.map(photo =>
-                                    <li key={photo.id} id={photo.id}>
-                                        <div>{photo.id} {photo.caption}</div>
-                                    </li>
-                                )}</div>
-                            <br/>
-                            <br/>
-                            <div>
-                                {photo_details.map(detail =>
-                                    <li key={detail.id} id={detail.id}>
-                                        <div>{detail.id} {detail.username} {detail.caption}</div>
-                                    </li>
-                                )}</div>
-                        </ul>
-                    </div>
-
 
                 </div>
             </React.Fragment>
